ac90aee842df1935c0c8c1815b225bf44952c43f
MaratNasyrov <maratnasyrov@gmail.com>
2021-02-01 14:04:44 +0300

    #9 Implement datasource mock

--- client/src/shared/stores/namespace/nsStore.ts
+++ client/src/shared/stores/namespace/nsStore.ts
@@ -1,26 +1,28 @@
 import {RootStore} from "../rootStore";
 import {action, observable, reaction} from "mobx";
+import {IDataSource} from "../../data/IDataSource";
+import {MemDataSource} from "../../data/memDataSource";
 
 export class NsStore
 {
-    rootStore: RootStore;
+    rootStore: RootStore
+    dataSource : IDataSource
 
     constructor(rootStore : RootStore) {
-        this.rootStore = rootStore;
-
+        this.rootStore = rootStore
+        this.dataSource = new MemDataSource()
         reaction(
             () => {},
             ()=>{}
-        );
+        )
     }
 
     @observable nsUnitMap = new Map();
     @observable nsFuncMap = new Map();
     @observable nsRelMap = new Map();
-    @observable loading = false;
+    @observable loading = false
 
     @action loadNs = async () => {
         this.loading = true;
-
     }
-}
+};
